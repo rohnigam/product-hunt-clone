9898187140cf22f1c0612a42c8479ac5b8bf178c
Rohitashwa Nigam (rohnigam) <rohnigam@cisco.com>
2018-08-21 07:30:55 +0530

    Product Hunt Clone Complete

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=20)
+    url = models.URLField()
+    publication_date = models.DateField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.CharField(max_length=2000)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
